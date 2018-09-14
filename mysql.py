78185197b8edb1d02982557f7ac7d81fd313efb9
maoxingda <39357378+maoxingda@users.noreply.github.com>
2018-09-15 01:24:51 +0800

    1、Excel表格读写

--- mysql.py
+++ mysql.py
@@ -1,12 +1,76 @@
 import pymysql
+import random
 
-dbTest = pymysql.connect(host='localhost', user='root', passwd='010203', db='test')
+db1 = pymysql.connect('localhost', 'root', '010203')
 
-cursor = dbTest.cursor()
+if db1:
+    cursor = db1.cursor()
 
-cursor.execute("select * from table1;")
+    # create database if not exists
+    cursor.execute('create database if not exists db1;')
 
-for row in cursor.fetchall():
-    print(row)
+    db1.commit()
 
-dbTest.close()
+    # select database
+    cursor.execute('use db1;')
+
+    db1.commit()
+
+    # create table if not exists
+    cursor.execute('create table if not exists tbl1('
+                   'id int unsigned auto_increment,'
+                   'name varchar(32) not null,'
+                   'sex char(1) not null,'
+                   'pNumber varchar(32),'
+                   'primary key(id));')
+
+    db1.commit()
+
+    # add
+    # name
+    alpha = 'abcdefghijklmnopqrstuvwxyz'
+    c = 0
+    name = ''
+    while c < 5:
+        c += 1
+        name += random.choice(alpha)
+
+    # sex
+    sex = random.randint(0, 1)
+    if 1 == sex:
+        sex = 'm'
+    elif 0 == sex:
+        sex = 'f'
+
+    # phoneNubmer
+    c = 0
+    pNumber = '1'
+    while c < 10:
+        c += 1
+        pNumber += str(random.randint(0, 9))
+
+    cursor.execute('insert into tbl1 '
+                   '(name, sex, pNumber) values'
+                   '("%s", "%s", "%s");' % (name, sex, pNumber))
+
+    db1.commit()
+
+    # delete
+    cursor.execute('delete from tbl1 where id = 1')
+
+    db1.commit()
+
+    # update
+    cursor.execute('update tbl1 set pNumber = "18001289144" where id = 1')
+
+    db1.commit()
+
+    # query
+    cursor.execute('select * from tbl1;')
+
+    for row in cursor.fetchall():
+        print(row, end=' ')
+        print()
+
+    # close
+    db1.close()
